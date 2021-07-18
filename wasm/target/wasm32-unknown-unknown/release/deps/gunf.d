0e2fc5e2f4198a6cafd77f52fb6fd3f2348baad4
spikeonmars <federodriguez.88@gmail.com>
2021-07-18 10:51:10 -0300

    Do not add target

--- wasm/target/wasm32-unknown-unknown/release/deps/gunf.d
+++
@@ -1,5 +0,0 @@
-/Users/federicorodriguez/projects/gunf/wasm/target/wasm32-unknown-unknown/release/deps/gunf.wasm: src/lib.rs
-
-/Users/federicorodriguez/projects/gunf/wasm/target/wasm32-unknown-unknown/release/deps/gunf.d: src/lib.rs
-
-src/lib.rs:
--- wasm/target/wasm32-unknown-unknown/release/gunf.d
+++
@@ -1,1 +0,0 @@
-/Users/federicorodriguez/projects/gunf/wasm/target/wasm32-unknown-unknown/release/gunf.wasm: /Users/federicorodriguez/projects/gunf/wasm/src/lib.rs
